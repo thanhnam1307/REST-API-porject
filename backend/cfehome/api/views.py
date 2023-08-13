514cd02e0dc8c97fce461bfde7afb2155da347be
nam nguyen thanh <bffkudo1412@gmail.com>
2023-08-13 13:34:25 +0700

    Django Model Instance to Dictionary

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,13 +1,13 @@
-from django.http import JsonResponse
+from django.http import JsonResponse ,HttpResponse
 import json
+from django.forms.models import model_to_dict
+
 from products.models import Product
 
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        data = model_to_dict(model_data,fields=['id' ,'title', 'price'])
     return JsonResponse(data)
+
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,10 +4,8 @@ import requests
 endpoint = "http://127.0.0.1:8000/api/"
 
 
-get_response = requests.get(endpoint ,params={"product_id":123}) #Http request
-#print(get_response.text)#print raw text response 
-#print(get_response.status_code)
-#HTTP Request ->HTML
-#REST API HTTP Request ->JSON
+get_response = requests.get(endpoint ,json={'product_id':123}) 
+
+
 print(get_response.json())
 #JavaScrip Object Nototion ~ Python Dict
