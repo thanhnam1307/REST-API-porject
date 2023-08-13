118a4e876d56d0de9bc40aefae8f6a80172d92ce
nam nguyen thanh <bffkudo1412@gmail.com>
2023-08-13 13:46:30 +0700

    Rest Framework View & Response

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,13 +1,17 @@
-from django.http import JsonResponse ,HttpResponse
-import json
 from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
         data = model_to_dict(model_data,fields=['id' ,'title', 'price'])
-    return JsonResponse(data)
-
+    return Response(data)
+ 
