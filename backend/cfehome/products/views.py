22dacadbd78657d5baaf269fb2d8e85e6a3c936e
nam nguyen thanh <bffkudo1412@gmail.com>
2023-08-15 15:42:32 +0700

    Token Authentication

--- backend/cfehome/products/views.py
+++ backend/cfehome/products/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
+from api.authentication import TokenAuthentication
 from .models import Product
 from .permissions import IsStaffEditorPermission
 from .serializers import ProductSerializer
@@ -10,7 +11,10 @@ from .serializers import ProductSerializer
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    authentication_classes = [authentication.SessionAuthentication]
+    authentication_classes = [
+        authentication.SessionAuthentication,
+        TokenAuthentication
+    ]
     permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
     
     def perform_create(self ,serializer):
--- py_client/create.py
+++ py_client/create.py
@@ -1,5 +1,7 @@
 import requests
 
+
+headers = {'Authorization': 'Bearer 56be721aeecb0ed611e2765e9253e2d62f0badfc'}
 #endpoint = "https://httpbin.org/status/200"
 endpoint = "http://127.0.0.1:8000/api/products/"
 
@@ -7,7 +9,7 @@ data = {
     "title" : "this field is done"
 }
 
-get_response = requests.post(endpoint ,json=data) 
+get_response = requests.post(endpoint, json=data, headers=headers) 
 
 print(get_response.json())
 #JavaScrip Object Nototion ~ Python Dict
