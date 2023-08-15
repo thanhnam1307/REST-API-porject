97b8dd05f5a156da3fbba3b96b0de66710133342
nam nguyen thanh <bffkudo1412@gmail.com>
2023-08-15 10:06:45 +0700

    Function Based Views For Create Retrieve or List

--- backend/cfehome/products/views.py
+++ backend/cfehome/products/views.py
@@ -1,9 +1,12 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductCreateAPIView(generics.CreateAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     
@@ -14,10 +17,44 @@ class ProductCreateAPIView(generics.CreateAPIView):
             content = title
         serializer.save(content=content)
 
-product_create_view = ProductCreateAPIView.as_view()
+product_list_create_view = ProductListCreateAPIView.as_view()
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 product_detail_view = ProductDetailAPIView.as_view()
+
+# class ProductListAPIView(generics.ListAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+# product_list_view = ProductListAPIView.as_view()
+
+
+@api_view(['GET' ,'POST'])
+def product_alt_view(request , pk = None ,*args ,**kwargs):
+    method = request.method 
+    if method == "GET":
+        if pk is not None:
+            #detail view 
+            obj = get_object_or_404(Product ,pk = pk)
+            data = ProductSerializer(obj ,many=False).data
+            return Response()
+        #list view
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset,many = True).data
+        return Response(data)
+
+    if method == "POST":
+        #create an item 
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({'invalid: not good data'},status=400)
+    
---
+++ py_client/list.py
@@ -0,0 +1,10 @@
+import requests
+
+#endpoint = "https://httpbin.org/status/200"
+endpoint = "http://127.0.0.1:8000/api/products/"
+
+
+get_response = requests.get(endpoint) 
+
+print(get_response.json())
+#JavaScrip Object Nototion ~ Python Dict
