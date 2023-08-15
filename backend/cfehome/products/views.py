215e7f631351fa2d11f8bd2125bc4fd5aba0e1a2
nam nguyen thanh <bffkudo1412@gmail.com>
2023-08-15 14:37:14 +0700

    Mixins and a Generic API View

--- backend/cfehome/products/views.py
+++ backend/cfehome/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics ,mixins
+from rest_framework import generics ,mixins ,permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
@@ -9,6 +9,7 @@ from .serializers import ProductSerializer
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     
     def perform_create(self ,serializer):
         title = serializer.validated_data.get('title')
