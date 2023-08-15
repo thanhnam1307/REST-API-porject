0cd1e57c06511bf44693c684a9be54a17e96ba87
nam nguyen thanh <bffkudo1412@gmail.com>
2023-08-15 17:25:51 +0700

    Model Serializer Create & Update Methods

--- backend/cfehome/products/serializers.py
+++ backend/cfehome/products/serializers.py
@@ -1,14 +1,18 @@
 from rest_framework import serializers
-
-
+from rest_framework.reverse import reverse
 from .models import Product
-
-
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+            view_name='product-detail',
+            lookup_field='pk'
+    )
     class Meta:
         model = Product
         fields = [
+            'url',
+            'edit_url',
             'pk',
             'title',
             'content',
@@ -17,9 +21,26 @@ class ProductSerializer(serializers.ModelSerializer):
             'my_discount',
         ]
 
-    def get_my_discount(self , obj):
-        if not hasattr(obj , 'id'):
+    # def create(self, validated_data):
+    #     # return Product.obejcts.create(**validated_data)
+    #     # email = validated_data.pop('email')
+    #     obj = super().create(validated_data)
+    #     # print(email, obj)
+    #     return obj
+
+    # def update(self, instance, validated_data):
+    #     email = validated_data.pop('email')
+    #     return super().update(instance, validated_data)
+
+    def get_edit_url(self, obj):
+        request = self.context.get('request') # self.request
+        if request is None:
             return None
-        if not isinstance(obj,Product):
-            return None    
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request) 
+    
+    def get_my_discount(self, obj):
+        if not hasattr(obj, 'id'):
+            return None
+        if not isinstance(obj, Product):
+            return None
         return obj.get_discount()
