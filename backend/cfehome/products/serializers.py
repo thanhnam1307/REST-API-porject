6d086c486d03766ff111e8845ec597c90eec6059
nam nguyen thanh <bffkudo1412@gmail.com>
2023-08-16 08:55:51 +0700

    Related Fields & Foreign Key Serializer

--- backend/cfehome/products/serializers.py
+++ backend/cfehome/products/serializers.py
@@ -1,11 +1,23 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 
-
+from api.serializers import UserPublicSerializer
 from .models import Product
 from . import validators
 
+class ProductInlineSerializer(serializers.Serializer):
+    url = serializers.HyperlinkedIdentityField(
+            view_name='product-detail',
+            lookup_field='pk',
+            read_only=True
+    )
+    title = serializers.CharField(read_only=True)
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    owner = UserPublicSerializer(source='user', read_only=True)
+    related_products = ProductInlineSerializer(source='user.product_set.all', read_only=True, many=True)
+    my_user_data = serializers.SerializerMethodField(read_only=True)
     my_discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(
@@ -13,11 +25,12 @@ class ProductSerializer(serializers.ModelSerializer):
             lookup_field='pk'
     )
     title = serializers.CharField(validators=[validators.validate_title_no_hello, validators.unique_product_title])
-    # email = serializers.CharField(source='user.email', read_only=True)
+    # email = serializers.EmailField(source='user.email', read_only=True)
     class Meta:
         model = Product
         fields = [
-            # 'user',
+            'owner',
+            # 'email',
             'url',
             'edit_url',
             'pk',
@@ -26,16 +39,14 @@ class ProductSerializer(serializers.ModelSerializer):
             'price',
             'sale_price',
             'my_discount',
+            'my_user_data',
+            'related_products',
         ]
 
-    #def validate_title(self, value):
-    #     request = self.context.get('request')
-    #     user = request.user
-    #     qs = Product.objects.filter(user=user, title__iexact=value)
-    #     if qs.exists():
-    #         raise serializers.ValidationError(f"{value} is already a product name.")
-    #     return value
-
+    def get_my_user_data(self, obj):
+        return {
+            "username": obj.user.username
+        }
 
     def get_edit_url(self, obj):
         request = self.context.get('request') # self.request
