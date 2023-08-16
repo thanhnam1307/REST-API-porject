d5637daf4e3cb8ab1b3508167c8071c4d43cf559
nam nguyen thanh <bffkudo1412@gmail.com>
2023-08-16 08:50:15 +0700

    Request User Data & Customize View Queryset

--- backend/cfehome/products/views.py
+++ backend/cfehome/products/views.py
@@ -3,11 +3,14 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
-from api.mixins import StaffEditorPermissionMixin
+from api.mixins import (
+    StaffEditorPermissionMixin,
+    UserQuerySetMixin)
 from .models import Product
 from .serializers import ProductSerializer
 
 class ProductListCreateAPIView(
+    UserQuerySetMixin,
     StaffEditorPermissionMixin,
     generics.ListCreateAPIView):
     queryset = Product.objects.all()
@@ -19,11 +22,19 @@ class ProductListCreateAPIView(
         content = serializer.validated_data.get('content')
         if content is None:
             content = title
-        serializer.save(content=content)
-
+        serializer.save(user=self.request.user, content=content)
+        # def get_queryset(self, *args, **kwargs):
+    #     qs = super().get_queryset(*args, **kwargs)
+    #     request = self.request
+    #     user = request.user
+    #     if not user.is_authenticated:
+    #         return Product.objects.none()
+    #     # print(request.user)
+    #     return qs.filter(user=request.user)    
 product_list_create_view = ProductListCreateAPIView.as_view()
 
-class ProductDetailAPIView(    
+class ProductDetailAPIView(
+    UserQuerySetMixin,     
     StaffEditorPermissionMixin,
     generics.RetrieveAPIView):
     queryset = Product.objects.all()
@@ -31,7 +42,10 @@ class ProductDetailAPIView(
 
 product_detail_view = ProductDetailAPIView.as_view()
 
-class ProductUpdateAPIView(generics.UpdateAPIView):
+class  ProductUpdateAPIView(
+    UserQuerySetMixin,
+    StaffEditorPermissionMixin,
+    generics.UpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     #permission_classes = [permissions.DjangoModelPermissions]
@@ -45,6 +59,7 @@ class ProductUpdateAPIView(generics.UpdateAPIView):
 product_update_view = ProductUpdateAPIView.as_view()
 
 class ProductDestroyAPIView(
+    UserQuerySetMixin,
     StaffEditorPermissionMixin,
     generics.DestroyAPIView):
     queryset = Product.objects.all()
